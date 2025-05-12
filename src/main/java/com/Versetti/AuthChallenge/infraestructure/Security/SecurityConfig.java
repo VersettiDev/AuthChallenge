03cf07b5cb383e5c40367a1180c9e2238d683a1d
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-12 04:24:59 -0300

    Add SecurityConfig class with basic security setup
    Introduced a new SecurityConfig class to configure Spring Security. Disabled CSRF and frame options for easier access to the H2 console, and permitted all requests for simplicity in initial development. Added beans for SecurityFilterChain and placeholders for future security enhancement.

---
+++ src/main/java/com/Versetti/AuthChallenge/infraestructure/Security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.Versetti.AuthChallenge_V10.infraestructure.security;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.RSAKey;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.disable())  // Necessário para o H2 Console
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()  // Permite acesso ao H2 Console
+                        .requestMatchers("/**").permitAll()
+                )
+                .securityMatcher("/**");
+
+        return http.build();
+    }
+}
