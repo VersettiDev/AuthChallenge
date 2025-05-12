d66ec30f45f5fb72234bf09ce75555ca31061910
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-12 04:15:22 -0300

    feat: Domain - User class

---
+++ src/main/java/com/Versetti/AuthChallenge/domain/User/User.java
@@ -0,0 +1,24 @@
+package com.Versetti.AuthChallenge.domain.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity (name = "users")
+@Table (name = "tb_users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+}
