c7922a9b97e783aa86394bdf2a9e846caee22fa5
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-12 04:16:28 -0300

    feat(Domain User): Define columns and constraints for User entity
    - Add unique and non-null constraints for username
    - Add non-null constraint for password
    - Add unique and non-null constraints for email

--- src/main/java/com/Versetti/AuthChallenge/domain/User/User.java
+++ src/main/java/com/Versetti/AuthChallenge/domain/User/User.java
@@ -18,7 +18,13 @@ public class User {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
 
     private Long id;
+
+    @Column (unique = true, nullable = false)
     private String username;
+
+    @Column (nullable = false)
     private String password;
+
+    @Column (unique = true, nullable = false)
     private String email;
 }
