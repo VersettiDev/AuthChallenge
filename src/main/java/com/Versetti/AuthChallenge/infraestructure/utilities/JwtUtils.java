24352900b4d2ba3e367df857d2a51227adc5dee2
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-12 04:49:58 -0300

    Add secret key and expiration time to JwtUtils
    Introduced a `secretToken` using HS256 algorithm and an `expirationTime` variable for token configuration. These changes are foundational for managing JWT token creation and validation.

--- src/main/java/com/Versetti/AuthChallenge/infraestructure/utilities/JwtUtils.java
+++ src/main/java/com/Versetti/AuthChallenge/infraestructure/utilities/JwtUtils.java
@@ -1,7 +1,17 @@
 package com.Versetti.AuthChallenge.infraestructure.utilities;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
 
 @Component
 public class JwtUtils {
+
+    private final Key secretToken = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final Long expirationTime = 1000 * 30 * 60;
+
+
 }
