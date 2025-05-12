414c7d8c509a805fb451aaf5473ec7de3143a88e
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-12 04:12:19 -0300

    Initial commit

---
+++ src/main/java/com/Versetti/AuthChallenge/AuthChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.Versetti.AuthChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthChallengeApplication.class, args);
+	}
+
+}
