7cafcc16e0cfb01cb894c6f6f12d3e3d2494d304
VersettiDev <brunomuryllo.nt@icloud.com>
2025-05-12 04:17:52 -0300

    Add UserRepository interface for user data access
    Implements a JpaRepository for User entities, providing methods to find users by username and email. This lays the groundwork for easier user data queries in the application.

---
+++ src/main/java/com/Versetti/AuthChallenge/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.Versetti.AuthChallenge.repositories;
+
+import com.Versetti.AuthChallenge.domain.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
